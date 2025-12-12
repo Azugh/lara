006e00c4b41f60d620a46d6ade9f3150db920ac0
Azugh <lybimov.egor@gmail.com>
2025-12-12 17:41:46 +0300

    register-302

--- app/Models/RegisterRequest.php
+++ app/Models/RegisterRequest.php
@@ -7,7 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class RegisterRequest extends Model
 {
     //
-    public $fillable = ['name', 'email', 'message', 'department', 'tel'];
+    public $fillable = ['name', 'email', 'message', 'department', 'tel',];
 
+    protected $hidden = ['isVerified'];
 
+    protected function casts(): array
+    {
+        return [
+            'verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
